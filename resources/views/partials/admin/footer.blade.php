817263ccdddb9b5810514173f88eaf5d224c2999
Davebube01 <davidabraham356@gmail.com>
2024-06-23 09:15:49 +0100

    created md-dashboard.blade and md-approved-template.blade for md requisition apporval

--- resources/views/partials/admin/footer.blade.php
+++ resources/views/partials/admin/footer.blade.php
@@ -32,6 +32,12 @@
 <script src="{{ asset('assets/js/plugins/choices.min.js') }}"></script>
 <script src="{{ asset('assets/js/plugins/flatpickr.min.js') }}"></script>
 
+{{-- For the modals --}}
+{{-- <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script> --}}
+<script src="{{ asset('assets/js/jquery-3.2.1.min.js') }}"></script>
+{{-- <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js"></script> --}}
+{{-- <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script> --}}
+<script src="{{ asset('assets/js/bootstrap-4.0.0.min.js') }}"></script>
 
 {{-- XLSX --}}
 
